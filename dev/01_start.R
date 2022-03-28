2128b4822f9a0db6696703168c7ce6e9fa993d0b
s220670 <s220670@dtu.dk>
2022-03-28 08:55:03 +0000

    setting things

--- dev/01_start.R
+++ dev/01_start.R
@@ -19,11 +19,11 @@
 ## to change the name in the app_sys() function in app_config.R /!\
 ##
 golem::fill_desc(
-  pkg_name = "myshinyapp", # The Name of the package containing the App
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App
+  pkg_name = "centraldogma", # The Name of the package containing the App
+  pkg_title = "Central dogma", # The Title of the package containing the App
   pkg_description = "PKG_DESC.", # The Description of the package containing the App
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
+  author_first_name = "Enrique", # Your First Name
+  author_last_name = "Martinez", # Your Last Name
   author_email = "AUTHOR@MAIL.COM", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional)
 )
